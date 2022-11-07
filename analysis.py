71ecc05a09068d157e915ff67cb33416f6ac3e17
Hannah Kirby <hvk.0903@gmail.com>
2022-11-07 16:59:58 -0600

    visualize dataframe and start analysis

--- analysis.py
+++ analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import sklearn as skl
 import matplotlib.pyplot as plt
 
 # get data, file has spaces and tabs as separators
@@ -6,18 +8,4 @@ seeds = pd.read_csv("data/seeds_dataset.txt", sep = '\s+|\t| ',
                     header = None, engine= "python",
                     names = ["area", "perimeter", "compact", "length", 
                     "width", "asymmetry", "groove", "seed_type"])
-                    
-# examine data
-print(seeds.iloc[0:9])
-
-# get mean and sd from input variables
-print("\nMean\n")
-print(seeds.iloc[:,0:7].mean())
-
-print("\nStandard Deviation \n")
-print(seeds.iloc[:,0:7].std())
-
-# boxplots to visualize above data
-seeds.iloc[:,0:7].plot(kind = 'box')
-plt.show()
 
