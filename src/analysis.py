97dd770e27e9ee787eba58e7b6804f3d0aa97c13
Hannah Kirby <hvk.0903@gmail.com>
2022-11-08 23:15:45 -0600

    knn analysis

--- src/analysis.py
+++ src/analysis.py
@@ -4,6 +4,9 @@ import sklearn as skl
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
 # get data, file has spaces and tabs as separators
 seeds = pd.read_csv("data/seeds_dataset.txt", sep = '\s+|\t| ', 
                     header = None, engine= "python",
@@ -12,7 +15,8 @@ seeds = pd.read_csv("data/seeds_dataset.txt", sep = '\s+|\t| ',
 
 # Use random forest to gain more insight on feature importance.
 
-X_train, X_test, y_train, y_test = train_test_split(seeds.iloc[:, :-1], seeds.iloc[:, -1:], test_size = 0.3, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(seeds.iloc[:, :-1], 
+                                seeds.iloc[:, -1:], test_size = 0.3, random_state=1)
 
 # feature scaling
 sc = skl.preprocessing.StandardScaler()
@@ -45,5 +49,37 @@ for f in range(X_train.shape[1]):
 # both high on the list of importance. groove also appears to
 # have a strong influence on seed_type
 
-# perform knn classification using top 3 important variables
+# subset seeds using only top 3 and top 2 most important features
+seeds_subset_3 = seeds[["groove", "area", "perimeter", "seed_type"]]
+seeds_subset_2 = seeds[["groove", "area", "seed_type"]]
+
+# for iterating over different seeds subsets
+seed_frames = [seeds, seeds_subset_3, seeds_subset_2]
+seed_names = ["All features", "Three features", "Two features"]
+
+# perform knn classification for each seeds subset and k 1-10 inclusive
+for i,seed in enumerate(seed_frames):
+        neighbors = range(1,11)
+        print(seed_names[i])
+        for n in neighbors:
+                X_train, X_test, y_train, y_test = train_test_split(seed.iloc[:, :-1], 
+                                seed.iloc[:, -1:], test_size = 0.3, random_state=1)
+
+                knn = KNeighborsClassifier(n_neighbors=n)
+
+                # fit model on training data
+                knn.fit(X_train, y_train.values.ravel())
+
+                # predict test values
+                y_pred = knn.predict(X_test)
+
+                # compare preds to test data to assess model accuracy
+                print("# neighbors: ", n, "\nAccuracy: ",
+                      sum(y_pred == y_test.values.ravel())/len(y_pred), "\n")
+
+# highest test accuracy from these combinations is 0.9365 from 
+# 2 features (groove and perimeter) and 4 neighbors.
+
+
+# 93% is decent but let's see if another classifier can more accurately fit the data.
 
