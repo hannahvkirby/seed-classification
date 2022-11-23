120cd0b409ee3f1b7089be8b09cc0d513974e7d0
Hannah Kirby <hvk.0903@gmail.com>
2022-11-22 18:20:48 -0600

    correlation of predictors and minor fixes

--- src/summary_stats.py
+++ src/summary_stats.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
 
 # get data, file has spaces and tabs as separators
@@ -12,10 +13,10 @@ seeds = pd.read_csv("data/seeds_dataset.txt", sep = '\s+|\t| ',
 print(seeds.iloc[0:9])
 
 # get mean and sd from input variables
-print("\nMean\n")
+print("\nMean")
 print(seeds.iloc[:,0:7].mean())
 
-print("\nStandard Deviation \n")
+print("\nStandard Deviation")
 print(seeds.iloc[:,0:7].std())
 
 # boxplots to visualize above data
@@ -29,3 +30,9 @@ plt.show()
 
 # seems like length and width have biggest disparities within
 # seed_type. Will continue exploring in analysis.py
+
+# let's also look at the correlation matrix of the predictor variables
+X=seeds.iloc[:, :-1]
+matrix = pd.DataFrame.cov(X)
+sns.heatmap(matrix, annot=True, fmt='g')
+plt.show()
